1da3b94f913507c4fcbca48bb8b65fc1ed47098e
Petr Horáček <phoracek@redhat.com>
2021-03-27 21:18:45 +0100

    Abstract Midi device
    Signed-off-by: Petr Horáček <phoracek@redhat.com>

--- src/main.rs
+++ src/main.rs
@@ -8,16 +8,15 @@ use panic_halt as _;
 use rtic::app;
 use stm32f3xx_hal::{
     gpio::{
-        gpioe::{MODER, OTYPER, PE13},
+        gpioa::{self, PA11, PA12},
+        gpioe::{self, PE13},
         Output, PushPull,
     },
     prelude::*,
+    rcc::Clocks,
     usb::{Peripheral, UsbBus, UsbBusType},
 };
-use usb_device::{
-    bus::{self, UsbBusAllocator},
-    prelude::*,
-};
+use usb_device::{bus::UsbBusAllocator, prelude::*};
 use usbd_midi::{
     data::{
         midi::{message::Message, notes::Note},
@@ -33,18 +32,14 @@ static mut USB_BUS: Option<UsbBusAllocator<UsbBusType>> = None;
 const APP: () = {
     struct Resources {
         led: Led,
-        usb_dev: UsbDevice<'static, UsbBusType>,
-        midi: MidiClass<'static, UsbBusType>,
+        midi: Midi,
     }
 
     #[init]
     fn init(cx: init::Context) -> init::LateResources {
         let mut rcc = cx.device.RCC.constrain();
+        let mut gpioa = cx.device.GPIOA.split(&mut rcc.ahb);
         let mut gpioe = cx.device.GPIOE.split(&mut rcc.ahb);
-
-        let mut led = Led::new(gpioe.pe13, &mut gpioe.moder, &mut gpioe.otyper);
-        led.set_low().unwrap();
-
         let mut flash = cx.device.FLASH.constrain();
 
         let clocks = rcc
@@ -54,72 +49,32 @@ const APP: () = {
             .pclk1(24u32.mhz())
             .pclk2(24u32.mhz())
             .freeze(&mut flash.acr);
-
         assert!(clocks.usbclk_valid());
 
-        // Configure the on-board LED (LD10, south red)
-        let mut led2 = gpioe
-            .pe12
-            .into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper);
-        led2.set_low().ok(); // Turn off
+        let mut led = Led::new(gpioe.pe13, &mut gpioe.moder, &mut gpioe.otyper);
+        led.set_low().unwrap();
 
-        let mut gpioa = cx.device.GPIOA.split(&mut rcc.ahb);
+        let midi = Midi::new(
+            gpioa.pa11,
+            gpioa.pa12,
+            cx.device.USB,
+            &clocks,
+            &mut gpioa.moder,
+            &mut gpioa.afrh,
+            &mut gpioa.otyper,
+        );
 
-        // F3 Discovery board has a pull-up resistor on the D+ line.
-        // Pull the D+ pin down to send a RESET condition to the USB bus.
-        // This forced reset is needed only for development, without it host
-        // will not reset your device when you upload new firmware.
-        let mut usb_dp = gpioa
-            .pa12
-            .into_push_pull_output(&mut gpioa.moder, &mut gpioa.otyper);
-        usb_dp.set_low().ok();
-        delay(clocks.sysclk().0 / 100);
-
-        let usb_dm = gpioa.pa11.into_af14(&mut gpioa.moder, &mut gpioa.afrh);
-        let usb_dp = usb_dp.into_af14(&mut gpioa.moder, &mut gpioa.afrh);
-
-        let usb = Peripheral {
-            usb: cx.device.USB,
-            pin_dm: usb_dm,
-            pin_dp: usb_dp,
-        };
-
-        unsafe {
-            USB_BUS = Some(UsbBus::new(usb));
-        }
-
-        let midi = MidiClass::new(unsafe { USB_BUS.as_ref().unwrap() });
-
-        let usb_dev = UsbDeviceBuilder::new(
-            unsafe { USB_BUS.as_ref().unwrap() },
-            UsbVidPid(0x16c0, 0x27dd),
-        )
-        .manufacturer("Zlosynth")
-        .product("Achordion")
-        .serial_number("0")
-        .device_class(USB_AUDIO_CLASS)
-        .device_sub_class(USB_MIDISTREAMING_SUBCLASS)
-        .build();
-
-        init::LateResources { led, usb_dev, midi }
+        init::LateResources { led, midi }
     }
 
-    #[task(binds = USB_HP_CAN_TX, resources = [usb_dev, midi, led])]
+    #[task(binds = USB_HP_CAN_TX, resources = [midi, led])]
     fn usb_tx(mut cx: usb_tx::Context) {
-        usb_poll(
-            &mut cx.resources.usb_dev,
-            &mut cx.resources.midi,
-            &mut cx.resources.led,
-        );
+        usb_poll(&mut cx.resources.midi, &mut cx.resources.led);
     }
 
-    #[task(binds = USB_LP_CAN_RX0, resources = [usb_dev, midi, led])]
+    #[task(binds = USB_LP_CAN_RX0, resources = [midi, led])]
     fn usb_rx0(mut cx: usb_rx0::Context) {
-        usb_poll(
-            &mut cx.resources.usb_dev,
-            &mut cx.resources.midi,
-            &mut cx.resources.led,
-        );
+        usb_poll(&mut cx.resources.midi, &mut cx.resources.led);
     }
 
     extern "C" {
@@ -132,7 +87,7 @@ pub struct Led {
 }
 
 impl Led {
-    pub fn new<T>(pe13: PE13<T>, moder: &mut MODER, otyper: &mut OTYPER) -> Self {
+    pub fn new<T>(pe13: PE13<T>, moder: &mut gpioe::MODER, otyper: &mut gpioe::OTYPER) -> Self {
         let led = pe13.into_push_pull_output(moder, otyper);
         Self { led }
     }
@@ -146,18 +101,70 @@ impl Led {
     }
 }
 
-fn usb_poll<B: bus::UsbBus>(
-    usb_dev: &mut UsbDevice<'static, B>,
-    midi: &mut MidiClass<'static, B>,
-    led: &mut Led,
-) {
-    if !usb_dev.poll(&mut [midi]) {
+pub struct Midi {
+    pub usb_device: UsbDevice<'static, UsbBusType>,
+    pub midi_class: MidiClass<'static, UsbBusType>,
+}
+
+impl Midi {
+    pub fn new<T1, T2>(
+        pa11: PA11<T1>,
+        pa12: PA12<T2>,
+        usb: stm32f3xx_hal::pac::USB,
+        clocks: &Clocks,
+        moder: &mut gpioa::MODER,
+        afrg: &mut gpioa::AFRH,
+        otyper: &mut gpioa::OTYPER,
+    ) -> Self {
+        // F3 Discovery board has a pull-up resistor on the D+ line.
+        // Pull the D+ pin down to send a RESET condition to the USB bus.
+        // This forced reset is needed only for development, without it host
+        // will not reset your device when you upload new firmware.
+        let mut usb_dp = pa12.into_push_pull_output(moder, otyper);
+        usb_dp.set_low().ok();
+        delay(clocks.sysclk().0 / 100);
+
+        let usb_dm = pa11.into_af14(moder, afrg);
+        let usb_dp = usb_dp.into_af14(moder, afrg);
+
+        let usb_peripheral = Peripheral {
+            usb,
+            pin_dm: usb_dm,
+            pin_dp: usb_dp,
+        };
+
+        unsafe {
+            USB_BUS = Some(UsbBus::new(usb_peripheral));
+        }
+
+        let midi_class = MidiClass::new(unsafe { USB_BUS.as_ref().unwrap() });
+
+        let usb_device = UsbDeviceBuilder::new(
+            unsafe { USB_BUS.as_ref().unwrap() },
+            UsbVidPid(0x16c0, 0x27dd),
+        )
+        .manufacturer("Zlosynth")
+        .product("Achordion")
+        .serial_number("0")
+        .device_class(USB_AUDIO_CLASS)
+        .device_sub_class(USB_MIDISTREAMING_SUBCLASS)
+        .build();
+
+        Self {
+            usb_device,
+            midi_class,
+        }
+    }
+}
+
+fn usb_poll(midi: &mut Midi, led: &mut Led) {
+    if !midi.usb_device.poll(&mut [&mut midi.midi_class]) {
         return;
     }
 
     let mut buffer = [0u8; 64];
 
-    if let Ok(size) = midi.read(&mut buffer) {
+    if let Ok(size) = midi.midi_class.read(&mut buffer) {
         let buffer_reader = MidiPacketBufferReader::new(&buffer, size);
         for packet in buffer_reader.into_iter() {
             if let Ok(packet) = packet {
