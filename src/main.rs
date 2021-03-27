18a8ba04ac1d5dc43bfbb1ab2de8096e36246ad8
Petr Horáček <phoracek@redhat.com>
2021-03-27 19:27:15 +0100

    Create an abstraction for the LED
    Signed-off-by: Petr Horáček <phoracek@redhat.com>

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
 #![no_main]
 #![no_std]
 
+use core::convert::Infallible;
+
 use panic_halt as _;
 use rtic::app;
 use rtic::cyccnt::{Instant, U32Ext as _};
 use stm32f3xx_hal::{
-    gpio::{gpioe::PE13, Output, PushPull},
+    gpio::{
+        gpioe::{MODER, OTYPER, PE13},
+        Output, PushPull,
+    },
     prelude::*,
 };
 
@@ -14,7 +19,7 @@ const PERIOD: u32 = 8_000_000;
 #[app(device = stm32f3xx_hal::pac, peripherals = true, monotonic = rtic::cyccnt::CYCCNT)]
 const APP: () = {
     struct Resources {
-        led: PE13<Output<PushPull>>,
+        led: Led,
     }
 
     #[init(schedule = [toggle_led])]
@@ -22,10 +27,7 @@ const APP: () = {
         let mut rcc = cx.device.RCC.constrain();
         let mut gpioe = cx.device.GPIOE.split(&mut rcc.ahb);
 
-        let mut led = gpioe
-            .pe13
-            .into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper);
-
+        let mut led = Led::new(gpioe.pe13, &mut gpioe.moder, &mut gpioe.otyper);
         led.set_low().unwrap();
 
         let now = cx.start;
@@ -45,3 +47,22 @@ const APP: () = {
         fn EXTI0();
     }
 };
+
+pub struct Led {
+    led: PE13<Output<PushPull>>,
+}
+
+impl Led {
+    pub fn new<T>(pe13: PE13<T>, moder: &mut MODER, otyper: &mut OTYPER) -> Self {
+        let led = pe13.into_push_pull_output(moder, otyper);
+        Self { led }
+    }
+
+    pub fn set_low(&mut self) -> Result<(), Infallible> {
+        self.led.set_low()
+    }
+
+    pub fn toggle(&mut self) -> Result<(), Infallible> {
+        self.led.toggle()
+    }
+}
