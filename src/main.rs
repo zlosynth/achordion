32fe0a7ae08eb3023a66fa31faa484a511dfd9ba
Petr Horáček <phoracek@redhat.com>
2021-03-27 18:45:49 +0100

    Toggle LED using HAL
    Signed-off-by: Petr Horáček <phoracek@redhat.com>

--- src/main.rs
+++ src/main.rs
@@ -4,46 +4,40 @@
 use panic_halt as _;
 use rtic::app;
 use rtic::cyccnt::{Instant, U32Ext as _};
+use stm32f3xx_hal::{
+    gpio::{gpioe::PE13, Output, PushPull},
+    prelude::*,
+};
 
 const PERIOD: u32 = 8_000_000;
 
 #[app(device = stm32f3xx_hal::pac, peripherals = true, monotonic = rtic::cyccnt::CYCCNT)]
 const APP: () = {
     struct Resources {
-        gpioe: stm32f3xx_hal::pac::GPIOE,
+        led: PE13<Output<PushPull>>,
     }
 
     #[init(schedule = [toggle_led])]
     fn init(cx: init::Context) -> init::LateResources {
-        cx.device
-            .RCC
-            .ahbenr
-            .write(|w| w.iopeen().set_bit());
+        let mut rcc = cx.device.RCC.constrain();
+        let mut gpioe = cx.device.GPIOE.split(&mut rcc.ahb);
 
-        cx.device.GPIOE.moder.write(|w| w.moder8().output());
+        let mut led = gpioe
+            .pe13
+            .into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper);
+
+        led.set_low().unwrap();
 
         let now = cx.start;
         cx.schedule.toggle_led(now + PERIOD.cycles()).unwrap();
 
-        init::LateResources {
-            gpioe: cx.device.GPIOE,
-        }
+        init::LateResources { led }
     }
 
-    #[task(schedule = [toggle_led], resources = [gpioe])]
+    #[task(schedule = [toggle_led], resources = [led])]
     fn toggle_led(cx: toggle_led::Context) {
-        static mut ENABLED: bool = false;
-
         let now = Instant::now();
-
-        if *ENABLED {
-            cx.resources.gpioe.odr.modify(|_, w| w.odr8().clear_bit());
-        } else {
-            cx.resources.gpioe.odr.modify(|_, w| w.odr8().set_bit());
-        }
-
-        *ENABLED = !*ENABLED;
-
+        cx.resources.led.toggle().unwrap();
         cx.schedule.toggle_led(now + PERIOD.cycles()).unwrap();
     }
 
