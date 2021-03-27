96be37c25d5cd36c1338c6f6fb009f5891b34671
Petr Horáček <phoracek@redhat.com>
2021-03-27 18:29:23 +0100

    Remove the button interrupt
    Make the code simpler while moving to HAL.
    
    Signed-off-by: Petr Horáček <phoracek@redhat.com>

--- src/main.rs
+++ src/main.rs
@@ -4,55 +4,29 @@
 use panic_halt as _;
 use rtic::app;
 use rtic::cyccnt::{Instant, U32Ext as _};
-use rtic::export::NVIC;
 
 const PERIOD: u32 = 8_000_000;
 
 #[app(device = stm32f3xx_hal::pac, peripherals = true, monotonic = rtic::cyccnt::CYCCNT)]
 const APP: () = {
     struct Resources {
-        gpioa: stm32f3xx_hal::pac::GPIOA,
         gpioe: stm32f3xx_hal::pac::GPIOE,
-        exti: stm32f3xx_hal::pac::EXTI,
     }
 
     #[init(schedule = [toggle_led])]
-    fn init(mut cx: init::Context) -> init::LateResources {
-        cx.core.DWT.enable_cycle_counter();
-
+    fn init(cx: init::Context) -> init::LateResources {
         cx.device
             .RCC
             .ahbenr
-            .write(|w| w.iopaen().set_bit().iopeen().set_bit());
-        cx.device.RCC.apb2enr.modify(|_, w| w.syscfgen().set_bit()); // enable clock for SYSCFG
+            .write(|w| w.iopeen().set_bit());
 
-        cx.device
-            .GPIOE
-            .moder
-            .write(|w| w.moder9().output().moder8().output());
-
-        cx.device.GPIOA.moder.modify(|_, w| w.moder0().input()); // moder0 corresponds to pin 0 on GPIOA
-        cx.device
-            .GPIOA
-            .pupdr
-            .modify(|_, w| unsafe { w.pupdr0().bits(0b10) }); // set mode to pull-down
-        cx.device
-            .SYSCFG
-            .exticr1
-            .modify(|_, w| unsafe { w.exti0().bits(0b000) }); // connect EXTI0 to PA0 pin
-        cx.device.EXTI.imr1.modify(|_, w| w.mr0().set_bit()); // unmask interrupt
-        cx.device.EXTI.rtsr1.modify(|_, w| w.tr0().set_bit()); // trigger on rising-edge
-        unsafe {
-            NVIC::unmask(stm32f3xx_hal::pac::Interrupt::EXTI0);
-        }
+        cx.device.GPIOE.moder.write(|w| w.moder8().output());
 
         let now = cx.start;
         cx.schedule.toggle_led(now + PERIOD.cycles()).unwrap();
 
         init::LateResources {
-            gpioa: cx.device.GPIOA,
             gpioe: cx.device.GPIOE,
-            exti: cx.device.EXTI,
         }
     }
 
@@ -73,21 +47,7 @@ const APP: () = {
         cx.schedule.toggle_led(now + PERIOD.cycles()).unwrap();
     }
 
-    #[task(binds = EXTI0, resources = [gpioe, exti])]
-    fn button_click(cx: button_click::Context) {
-        cx.resources.exti.pr1.modify(|_, w| w.pr0().set_bit()); // clear the EXTI line 0 pending bit
-
-        cx.resources.gpioe.odr.modify(|r, w| {
-            let led4 = r.odr9().bit();
-            if led4 {
-                w.odr9().clear_bit()
-            } else {
-                w.odr9().set_bit()
-            }
-        });
-    }
-
     extern "C" {
-        fn EXTI1();
+        fn EXTI0();
     }
 };
