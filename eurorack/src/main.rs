637e30fbcc54b5a2497b857fd5de64fc5e17e406
Petr Horáček <phoracek@redhat.com>
2021-03-30 22:21:47 +0200

    Move Midi to its own module
    Signed-off-by: Petr Horáček <phoracek@redhat.com>

--- eurorack/src/main.rs
+++ eurorack/src/main.rs
@@ -2,29 +2,24 @@
 #![no_std]
 
 mod led;
+mod midi;
 
-use cortex_m::asm::delay;
 use panic_halt as _;
 use rtic::app;
-use stm32f3xx_hal::{
-    gpio::gpioa::{self, PA11, PA12},
-    prelude::*,
-    rcc::Clocks,
-    usb::{Peripheral, UsbBus, UsbBusType},
-};
-use usb_device::{bus::UsbBusAllocator, prelude::*};
+use stm32f3xx_hal::{prelude::*, usb::UsbBusType};
+use usb_device::bus::UsbBusAllocator;
 use usbd_midi::{
     data::{
         midi::{message::Message, notes::Note},
-        usb::constants::{USB_AUDIO_CLASS, USB_MIDISTREAMING_SUBCLASS},
         usb_midi::{
             midi_packet_reader::MidiPacketBufferReader, usb_midi_event_packet::UsbMidiEventPacket,
         },
     },
-    midi_device::{MidiClass, MAX_PACKET_SIZE},
+    midi_device::MAX_PACKET_SIZE,
 };
 
 use crate::led::Led;
+use crate::midi::Midi;
 
 static mut USB_BUS: Option<UsbBusAllocator<UsbBusType>> = None;
 
@@ -61,6 +56,7 @@ const APP: () = {
             &mut gpioa.moder,
             &mut gpioa.afrh,
             &mut gpioa.otyper,
+            unsafe { &mut USB_BUS },
         );
 
         init::LateResources { led, midi }
@@ -80,64 +76,6 @@ const APP: () = {
         fn EXTI0();
     }
 };
-
-pub struct Midi {
-    pub usb_device: UsbDevice<'static, UsbBusType>,
-    pub midi_class: MidiClass<'static, UsbBusType>,
-}
-
-impl Midi {
-    pub fn new<T1, T2>(
-        pa11: PA11<T1>,
-        pa12: PA12<T2>,
-        usb: stm32f3xx_hal::pac::USB,
-        clocks: &Clocks,
-        moder: &mut gpioa::MODER,
-        afrg: &mut gpioa::AFRH,
-        otyper: &mut gpioa::OTYPER,
-    ) -> Self {
-        assert!(clocks.usbclk_valid());
-
-        // F3 Discovery board has a pull-up resistor on the D+ line.
-        // Pull the D+ pin down to send a RESET condition to the USB bus.
-        // This forced reset is needed only for development, without it host
-        // will not reset your device when you upload new firmware.
-        let mut usb_dp = pa12.into_push_pull_output(moder, otyper);
-        usb_dp.set_low().ok();
-        delay(clocks.sysclk().0 / 100);
-
-        let usb_dm = pa11.into_af14(moder, afrg);
-        let usb_dp = usb_dp.into_af14(moder, afrg);
-
-        let usb_peripheral = Peripheral {
-            usb,
-            pin_dm: usb_dm,
-            pin_dp: usb_dp,
-        };
-
-        unsafe {
-            USB_BUS = Some(UsbBus::new(usb_peripheral));
-        }
-
-        let midi_class = MidiClass::new(unsafe { USB_BUS.as_ref().unwrap() });
-
-        let usb_device = UsbDeviceBuilder::new(
-            unsafe { USB_BUS.as_ref().unwrap() },
-            UsbVidPid(0x16c0, 0x27dd),
-        )
-        .manufacturer("Zlosynth")
-        .product("Achordion")
-        .serial_number("0")
-        .device_class(USB_AUDIO_CLASS)
-        .device_sub_class(USB_MIDISTREAMING_SUBCLASS)
-        .build();
-
-        Self {
-            usb_device,
-            midi_class,
-        }
-    }
-}
 
 fn midi_poll(midi: &mut Midi, led: &mut Led) {
     if !midi.usb_device.poll(&mut [&mut midi.midi_class]) {
