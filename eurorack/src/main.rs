ad5e7ae8729c28584a4bc79627385e654ad13b8c
Petr Horáček <phoracek@redhat.com>
2021-04-04 14:21:35 +0200

    Control frequency using MIDI
    Signed-off-by: Petr Horáček <phoracek@redhat.com>

--- eurorack/src/main.rs
+++ eurorack/src/main.rs
@@ -14,9 +14,12 @@ use stm32f3xx_hal::dma::{self, Direction, Increment, Priority};
 use stm32f3xx_hal::gpio::{Edge, Gpioa, Input, Pin};
 use stm32f3xx_hal::pac::{Interrupt, NVIC};
 use stm32f3xx_hal::prelude::*;
+use stm32f3xx_hal::serial::{self, Serial};
 use stm32f3xx_hal::timer::Timer;
 use typenum::UTerm;
 
+use achordion_lib::midi::instrument::Instrument as MidiInstrument;
+use achordion_lib::midi::parser::MidiParser;
 use achordion_lib::wavetable;
 
 use crate::hal::prelude::*;
@@ -31,7 +34,10 @@ const APP: () = {
     struct Resources {
         dma_ch3: C3,
         button: Pin<Gpioa, UTerm, Input>,
-        #[init(40.0)]
+        serial_rx: serial::Rx<stm32f3xx_hal::pac::USART1>,
+        midi_parser: MidiParser,
+        midi_instrument: MidiInstrument,
+        #[init(0.0)]
         frequency: f32,
     }
 
@@ -39,6 +45,7 @@ const APP: () = {
     fn init(cx: init::Context) -> init::LateResources {
         let mut rcc = cx.device.RCC.constrain();
         let mut gpioa = cx.device.GPIOA.split(&mut rcc.ahb);
+        let mut gpioc = cx.device.GPIOC.split(&mut rcc.ahb);
         let mut exti = cx.device.EXTI;
         let mut syscfg = cx.device.SYSCFG.constrain(&mut rcc.apb2);
         let mut flash = cx.device.FLASH.constrain();
@@ -47,8 +54,8 @@ const APP: () = {
         let clocks = rcc.cfgr.freeze(&mut flash.acr);
 
         // Start periodic timer on TIM2
-        let mut t2 = Timer::tim2(cx.device.TIM2, SAMPLE_RATE.Hz(), clocks, &mut rcc.apb1);
-        t2.reset_on_overflow();
+        let mut tim2 = Timer::tim2(cx.device.TIM2, SAMPLE_RATE.Hz(), clocks, &mut rcc.apb1);
+        tim2.reset_on_overflow();
 
         // Configure DAC, disable buffer for better SNR and request data from DMA
         let mut dac = cx.device.DAC1.constrain(
@@ -91,9 +98,45 @@ const APP: () = {
         let interrupt_num = button.nvic();
         unsafe { NVIC::unmask(interrupt_num) };
 
+        // Configure USART for MIDI
+        let pins = (
+            gpioc
+                .pc4
+                .into_af7_push_pull(&mut gpioc.moder, &mut gpioc.otyper, &mut gpioc.afrl),
+            gpioc
+                .pc5
+                .into_af7_push_pull(&mut gpioc.moder, &mut gpioc.otyper, &mut gpioc.afrl),
+        );
+        let mut serial = Serial::usart1(cx.device.USART1, pins, 9600.Bd(), clocks, &mut rcc.apb2);
+        serial.listen(serial::Event::Rxne);
+        unsafe { NVIC::unmask(Interrupt::USART1_EXTI25) };
+        let (_tx, rx) = serial.split();
+
         init::LateResources {
             button,
             dma_ch3: dma2.ch3,
+            serial_rx: rx,
+            midi_parser: MidiParser::new(),
+            midi_instrument: MidiInstrument::new(),
+        }
+    }
+
+    #[task(binds = USART1_EXTI25, resources = [frequency, serial_rx, midi_parser, midi_instrument])]
+    fn usart1(mut cx: usart1::Context) {
+        loop {
+            match cx.resources.serial_rx.read() {
+                Ok(x) => {
+                    if let Some(m) = cx.resources.midi_parser.parse_byte(x) {
+                        let state = cx.resources.midi_instrument.reconcile(m);
+                        cx.resources.frequency.lock(|frequency| {
+                            *frequency = state.frequency;
+                        });
+                    }
+                }
+                _ => {
+                    return;
+                }
+            }
         }
     }
 
@@ -164,8 +207,8 @@ fn audio_callback(buffer: &mut [u32; DMA_LENGTH], length: usize, offset: usize, 
 
     for t in 0..length {
         let index = (phase * wt_length as f32) as usize;
-        let channel_1 = wt_sin[index] as u32;
-        let channel_2 = wt_saw[index] as u32;
+        let channel_1 = wt_saw[index] as u32;
+        let channel_2 = wt_sin[index] as u32;
 
         let frame = t + (offset * length);
         buffer[frame] = (channel_2 << 16) + channel_1;
