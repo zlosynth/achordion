bdde7244c87a6768f3320ea2620d62dd96ea250c
Petr Horáček <phoracek@redhat.com>
2021-04-05 16:16:36 +0200

    Implement simple bandlimitation in the oscillator
    Signed-off-by: Petr Horáček <phoracek@redhat.com>

--- lib/src/oscillator.rs
+++ lib/src/oscillator.rs
@@ -5,27 +5,36 @@ pub struct Oscillator<'a> {
     pub frequency: f32,
     phase: f32,
     sample_rate: f32,
-    wavetable: &'a [u16],
-    wavetable_length: f32,
+    wavetable_factors: &'a [&'a [u16]],
 }
 
 impl<'a> Oscillator<'a> {
-    pub fn new(wavetable: &'a [u16], sample_rate: u32) -> Self {
+    pub fn new(wavetable_factors: &'a [&[u16]], sample_rate: u32) -> Self {
         Self {
             frequency: 0.0,
             phase: 0.0,
             sample_rate: sample_rate as f32,
-            wavetable,
-            wavetable_length: wavetable.len() as f32,
+            wavetable_factors,
         }
     }
 
     pub fn populate(&mut self, buffer: &mut [u16]) {
         let interval_in_samples = self.frequency / self.sample_rate;
 
+        let factor = {
+            let mut factor = 0;
+            let mut block = self.sample_rate as f32 / 4.0;
+            while self.frequency < block && factor < self.wavetable_factors.len() - 1 {
+                block /= 2.0;
+                factor += 1;
+            }
+            factor
+        };
+        let wavetable = self.wavetable_factors[factor];
+
         for x in buffer.iter_mut() {
-            let position = self.phase * self.wavetable_length;
-            *x = linear_interpolation(self.wavetable, position);
+            let position = self.phase * wavetable.len() as f32;
+            *x = linear_interpolation(wavetable, position);
             self.phase += interval_in_samples;
             if self.phase >= 1.0 {
                 self.phase -= 1.0;
@@ -54,16 +63,17 @@ mod tests {
     use super::*;
 
     const WAVETABLE: [u16; 8] = [8, 10, 12, 14, 0, 2, 4, 6];
+    const FACTORS: [&[u16]; 1] = [&WAVETABLE];
     const SAMPLE_RATE: u32 = 8;
 
     #[test]
     fn initialize() {
-        let _oscillator = Oscillator::new(&WAVETABLE, SAMPLE_RATE);
+        let _oscillator = Oscillator::new(&FACTORS, SAMPLE_RATE);
     }
 
     #[test]
     fn populate() {
-        let mut oscillator = Oscillator::new(&WAVETABLE, SAMPLE_RATE);
+        let mut oscillator = Oscillator::new(&FACTORS, SAMPLE_RATE);
         let mut buffer = [0; 8];
 
         oscillator.frequency = 1.0;
@@ -77,7 +87,7 @@ mod tests {
 
     #[test]
     fn interpolation() {
-        let mut oscillator = Oscillator::new(&WAVETABLE, SAMPLE_RATE);
+        let mut oscillator = Oscillator::new(&FACTORS, SAMPLE_RATE);
         let mut buffer = [0; 8];
 
         oscillator.frequency = 0.5;
