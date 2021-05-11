525c92493df541a62e3ad8fbc17826387d413a05
Petr Horacek <hrck@protonmail.com>
2021-05-11 19:21:09 +0200

    Make it clear that we deal with voct
    Signed-off-by: Petr Horacek <hrck@protonmail.com>

--- puredata/src/lib.rs
+++ puredata/src/lib.rs
@@ -63,7 +63,7 @@ pub unsafe extern "C" fn achordion_tilde_setup() {
         callback = perform
     );
 
-    register_set_frequency_method(class);
+    register_set_voct_method(class);
     register_set_mode_method(class);
     register_set_root_method(class);
     register_set_wavetable_method(class);
@@ -98,13 +98,13 @@ unsafe extern "C" fn new() -> *mut c_void {
     class as *mut c_void
 }
 
-unsafe fn register_set_frequency_method(class: *mut pd_sys::_class) {
+unsafe fn register_set_voct_method(class: *mut pd_sys::_class) {
     pd_sys::class_addmethod(
         class,
         Some(std::mem::transmute::<
             unsafe extern "C" fn(*mut Class, pd_sys::t_float),
             _,
-        >(set_frequency)),
+        >(set_voct)),
         &mut pd_sys::s_float,
         pd_sys::t_atomtype::A_FLOAT,
         0,
@@ -150,8 +150,7 @@ unsafe fn register_set_root_method(class: *mut pd_sys::_class) {
     );
 }
 
-unsafe extern "C" fn set_frequency(class: *mut Class, value: pd_sys::t_float) {
-    // TODO: Convert frequency to voct
+unsafe extern "C" fn set_voct(class: *mut Class, value: pd_sys::t_float) {
     (*class).instrument.set_voct(value.clamp(0.0, 20.0));
 }
 
