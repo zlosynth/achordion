20d40a04c97d5f0c734c7f6cf5ddd6d96c2a4515
Petr Horacek <hrck@protonmail.com>
2021-05-15 16:06:19 +0200

    Use interpolation to pick a detune
    Signed-off-by: Petr Horacek <hrck@protonmail.com>

--- lib/src/instrument.rs
+++ lib/src/instrument.rs
@@ -19,6 +19,17 @@ const CHORDS: [[i8; 3]; 9] = [
     [1, 2, 9],
 ];
 
+const DETUNES: [[f32; 3]; 8] = [
+    [0.0, 0.0, 0.0],
+    [0.1, 0.1, 0.1],
+    [0.5, 0.0, 0.0],
+    [0.5, 0.0, 0.0],
+    [0.5, 0.5, 0.5],
+    [4.0 / 3.0, 4.0 / 3.0, 4.0 / 3.0],
+    [4.0 / 3.0, 4.0 / 3.0, 4.0 / 3.0],
+    [5.0 / 2.0, 5.0 / 2.0, 5.0 / 2.0],
+];
+
 const DEGREES_IN_INSTRUMENT: usize = 3;
 
 pub struct Instrument<'a> {
@@ -124,6 +135,7 @@ const OSCILLATORS_IN_DEGREE: usize = 2;
 
 struct Degree<'a> {
     frequency: f32,
+    detune_base: f32,
     detune_intensity: f32,
     detune_amplitude: f32,
     oscillators: [Oscillator<'a>; OSCILLATORS_IN_DEGREE],
@@ -133,6 +145,7 @@ impl<'a> Degree<'a> {
     pub fn new(wavetables: &'a [&'a Wavetable], sample_rate: u32) -> Self {
         Self {
             frequency: 0.0,
+            detune_base: 1.0,
             detune_intensity: 0.0,
             detune_amplitude: 0.0,
             oscillators: [
@@ -168,9 +181,11 @@ impl<'a> Degree<'a> {
         self.oscillators[0].frequency = self.frequency;
 
         for (i, pair) in self.oscillators[start..].chunks_exact_mut(2).enumerate() {
+            // TODO: Detune base
             let detune = 0.01 * self.detune_intensity * ((i + 1) / distance) as f32;
-            pair[0].frequency = self.frequency * (1.0 - detune);
-            pair[1].frequency = self.frequency * (1.0 + detune);
+            pair[0].frequency = self.frequency * (1.0 - detune) * (1.0 / self.detune_base) * (i + 1) as f32;
+            pair[1].frequency =
+                self.frequency * (1.0 + detune) * self.detune_base * (i + 1) as f32;
         }
     }
 
@@ -194,5 +209,46 @@ fn zero_slice(slice: &mut [u16]) {
     unsafe {
         let p = slice.as_mut_ptr();
         ptr::write_bytes(p, 0, slice.len());
+    }
+}
+
+fn detune_interpolation<const N: usize>(detunes: &[[f32; N]], position: f32) -> [f32; N] {
+    let index = position as usize;
+    let next_index = usize::min(index + 1, DETUNES.len() - 1);
+    let remainder = position - index as f32;
+
+    let mut detune = [0.0; N];
+
+    for i in 0..N {
+        let value = detunes[index][i];
+        let delta_to_next = detunes[next_index][i] - detunes[index][i];
+        detune[i] = value + delta_to_next * remainder;
+    }
+
+    detune
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn replace_slice_contents_with_zeros() {
+        let mut slice = [1, 2, 3];
+        zero_slice(&mut slice);
+        assert_eq!(slice, [0, 0, 0]);
+    }
+
+    #[test]
+    fn detune_interpolation_in_the_middle() {
+        let detunes = [
+            [0.0, 0.0],
+            [1.0, 2.0],
+        ];
+
+        let detune = detune_interpolation(&detunes, 0.5);
+
+        assert_relative_eq!(detune[0], 0.5);
+        assert_relative_eq!(detune[1], 1.0);
     }
 }
