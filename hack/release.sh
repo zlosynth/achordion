ee405ade89361e709a452ece0b77ce0b8c30d5ba
Petr Horacek <hrck@protonmail.com>
2021-07-05 18:07:30 +0200

    Automate bump of changelog's versioning
    Signed-off-by: Petr Horacek <hrck@protonmail.com>

--- hack/release.sh
+++ hack/release.sh
@@ -3,6 +3,7 @@ set -euo pipefail
 
 version=${1}
 
+sed -i "s/## Unreleased/## Unreleased\n\n## ${version}/" CHANGELOG.md
 sed -i "s/version =.* # hack\/release.sh$/version = \"${version}\" # hack\/release.sh/" eurorack/Cargo.toml
 sed -i "s/version =.* # hack\/release.sh$/version = \"${version}\" # hack\/release.sh/" lib/Cargo.toml
 sed -i "s/version =.* # hack\/release.sh$/version = \"${version}\" # hack\/release.sh/" puredata/Cargo.toml
