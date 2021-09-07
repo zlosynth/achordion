3be3eaaba7a01babdbfc4378d63210978678e046
Petr Horacek <hrck@protonmail.com>
2021-09-07 21:55:28 +0200

    Generate Pot abstraction through a macro
    This makes it possible to define Pot for any ADC channel.
    
    Signed-off-by: Petr Horacek <hrck@protonmail.com>

--- eurorack/src/system/pot.rs
+++ eurorack/src/system/pot.rs
@@ -11,30 +11,36 @@ pub struct Pot<P> {
     movement_detector: ControlBuffer<124>,
 }
 
-impl<P: Channel<ADC1, ID = u8>> Pot<P> {
-    pub fn new(pin: P) -> Self {
-        Self {
-            pin,
-            position_filter: ControlBuffer::new(),
-            movement_detector: ControlBuffer::new(),
+macro_rules! pot {
+    ($adc:ident) => {
+        impl<P: Channel<$adc, ID = u8>> Pot<P> {
+            pub fn new(pin: P) -> Self {
+                Self {
+                    pin,
+                    position_filter: ControlBuffer::new(),
+                    movement_detector: ControlBuffer::new(),
+                }
+            }
+
+            pub fn sample(&mut self, adc: &mut Adc<$adc, Enabled>) {
+                let sample: u32 = adc.read(&mut self.pin).unwrap();
+                let transposed_sample = transpose_adc(sample as f32, adc.max_sample());
+                self.position_filter.write(transposed_sample);
+                self.movement_detector.write(transposed_sample);
+            }
+
+            pub fn value(&self) -> f32 {
+                self.position_filter.read()
+            }
+
+            pub fn active(&self) -> bool {
+                self.movement_detector.traveled() > 0.005
+            }
         }
-    }
-
-    pub fn sample(&mut self, adc: &mut Adc<ADC1, Enabled>) {
-        let sample: u32 = adc.read(&mut self.pin).unwrap();
-        let transposed_sample = transpose_adc(sample as f32, adc.max_sample());
-        self.position_filter.write(transposed_sample);
-        self.movement_detector.write(transposed_sample);
-    }
-
-    pub fn value(&self) -> f32 {
-        self.position_filter.read()
-    }
-
-    pub fn active(&self) -> bool {
-        self.movement_detector.traveled() > 0.005
-    }
+    };
 }
+
+pot!(ADC1);
 
 fn transpose_adc(sample: f32, max_sample: u32) -> f32 {
     (max_sample as f32 - sample) / max_sample as f32
