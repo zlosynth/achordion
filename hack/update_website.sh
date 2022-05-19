c71f365f5c6b19b0ab7dc50a6656ca045dde9615
Petr Horacek <hrck@protonmail.com>
2022-05-19 22:53:26 +0200

    Fix script copying manuals to website
    Signed-off-by: Petr Horacek <hrck@protonmail.com>

--- hack/update_website.sh
+++ hack/update_website.sh
@@ -3,6 +3,6 @@ set -euo pipefail
 
 make manual
 
-cp manual/user/manual_digital.pdf ../zlosynth.github.io/docs/achordion/achordion-user-manual.pdf
-cp manual/user/manual_papare.pdf ../zlosynth.github.io/docs/achordion/achordion-user-manual-leaflet.pdf
-cp manual/build/manual.pdf ../zlosynth.github.io/docs/achordion/achordion-build-manual.pdf
+cp manual/user/manual_digital.pdf ../zlosynth.github.io/docs/achordion-user-manual.pdf
+cp manual/user/manual_paper.pdf ../zlosynth.github.io/docs/achordion-user-manual-leaflet.pdf
+cp manual/build/manual.pdf ../zlosynth.github.io/docs/achordion-build-manual.pdf
