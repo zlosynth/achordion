9da3649664ff5ba43e49ff4e8e66b016573b4317
Petr Horacek <hrck@protonmail.com>
2022-07-19 19:36:44 +0200

    Fix profiling macros
    Signed-off-by: Petr Horacek <hrck@protonmail.com>

--- eurorack/src/profiling.rs
+++ eurorack/src/profiling.rs
@@ -2,57 +2,57 @@
 /// through oscilloscope. They are not using HAL, but instead they access the
 /// hardware directly to save troubles passing resources.
 ///
-/// A: Daisy pin 1, PB12
-/// B: Daisy pin 2, PC11
-/// C: Daisy pin 3, PC10
+/// A: Daisy Patch SM pin A3, PA0
+/// B: Daisy Patch SM pin A8, PB14
+/// C: Daisy Patch SM pin A9, PB15
 #[macro_export]
 macro_rules! profile {
     (a, on) => {{
-        use daisy_bsp::hal::pac::{GPIOB, RCC};
+        use daisy::hal::pac::{GPIOA, RCC};
         let rcc = unsafe { &*RCC::ptr() };
-        let gpiob = unsafe { &*GPIOB::ptr() };
+        let gpiob = unsafe { &*GPIOA::ptr() };
         rcc.ahb4enr.modify(|_, w| w.gpioben().set_bit());
-        gpiob.moder.modify(|_, w| w.moder12().output());
-        gpiob.odr.modify(|_, w| w.odr12().set_bit());
+        gpiob.moder.modify(|_, w| w.moder0().output());
+        gpiob.odr.modify(|_, w| w.odr0().set_bit());
     }};
     (a, off) => {{
-        use daisy_bsp::hal::pac::{GPIOB, RCC};
+        use daisy::hal::pac::{GPIOA, RCC};
         let rcc = unsafe { &*RCC::ptr() };
-        let gpiob = unsafe { &*GPIOB::ptr() };
+        let gpiob = unsafe { &*GPIOA::ptr() };
         rcc.ahb4enr.modify(|_, w| w.gpioben().set_bit());
-        gpiob.moder.modify(|_, w| w.moder12().output());
-        gpiob.odr.modify(|_, w| w.odr12().clear_bit());
+        gpiob.moder.modify(|_, w| w.moder0().output());
+        gpiob.odr.modify(|_, w| w.odr0().clear_bit());
     }};
     (b, on) => {{
-        use daisy_bsp::hal::pac::{GPIOC, RCC};
+        use daisy::hal::pac::{GPIOB, RCC};
         let rcc = unsafe { &*RCC::ptr() };
-        let gpioc = unsafe { &*GPIOC::ptr() };
+        let gpioc = unsafe { &*GPIOB::ptr() };
         rcc.ahb4enr.modify(|_, w| w.gpiocen().set_bit());
-        gpioc.moder.modify(|_, w| w.moder11().output());
-        gpioc.odr.modify(|_, w| w.odr11().set_bit());
+        gpioc.moder.modify(|_, w| w.moder14().output());
+        gpioc.odr.modify(|_, w| w.odr14().set_bit());
     }};
     (b, off) => {{
-        use daisy_bsp::hal::pac::{GPIOC, RCC};
+        use daisy::hal::pac::{GPIOB, RCC};
         let rcc = unsafe { &*RCC::ptr() };
-        let gpioc = unsafe { &*GPIOC::ptr() };
+        let gpioc = unsafe { &*GPIOB::ptr() };
         rcc.ahb4enr.modify(|_, w| w.gpiocen().set_bit());
-        gpioc.moder.modify(|_, w| w.moder11().output());
-        gpioc.odr.modify(|_, w| w.odr11().clear_bit());
+        gpioc.moder.modify(|_, w| w.moder14().output());
+        gpioc.odr.modify(|_, w| w.odr14().clear_bit());
     }};
     (c, on) => {{
-        use daisy_bsp::hal::pac::{GPIOC, RCC};
+        use daisy::hal::pac::{GPIOB, RCC};
         let rcc = unsafe { &*RCC::ptr() };
-        let gpioc = unsafe { &*GPIOC::ptr() };
+        let gpioc = unsafe { &*GPIOB::ptr() };
         rcc.ahb4enr.modify(|_, w| w.gpiocen().set_bit());
-        gpioc.moder.modify(|_, w| w.moder10().output());
-        gpioc.odr.modify(|_, w| w.odr10().set_bit());
+        gpioc.moder.modify(|_, w| w.moder15().output());
+        gpioc.odr.modify(|_, w| w.odr15().set_bit());
     }};
     (c, off) => {{
-        use daisy_bsp::hal::pac::{GPIOC, RCC};
+        use daisy::hal::pac::{GPIOB, RCC};
         let rcc = unsafe { &*RCC::ptr() };
-        let gpioc = unsafe { &*GPIOC::ptr() };
+        let gpioc = unsafe { &*GPIOB::ptr() };
         rcc.ahb4enr.modify(|_, w| w.gpiocen().set_bit());
-        gpioc.moder.modify(|_, w| w.moder10().output());
-        gpioc.odr.modify(|_, w| w.odr10().clear_bit());
+        gpioc.moder.modify(|_, w| w.moder15().output());
+        gpioc.odr.modify(|_, w| w.odr15().clear_bit());
     }};
 }
